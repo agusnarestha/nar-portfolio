970515ea26096bd46ece76089fe7a6eabc519cae
Agus Narestha <63548030+agusnarestha@users.noreply.github.com>
2023-11-23 21:25:37 +0800

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  rules: {
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-page-custom-font": "off",
+  },
 };
 
 module.exports = nextConfig;
