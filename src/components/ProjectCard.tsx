4f26b74e46f34c3a1f345243ad1cbcaa53112662
Agus Narestha <63548030+agusnarestha@users.noreply.github.com>
2024-05-18 03:02:52 +0800

    #Feat:  dynamic projects (#14)
    * #1 (#12)
    
    * fix: style
    
    * fix: style container
    
    * add: asset img
    
    * feat: dynamic projects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,15 +1,29 @@
 import Image from "next/image";
 
-const ProjectCard = () => {
+type ProjectCardProps = {
+  title: string;
+  description: string;
+  imageUrl: string;
+  linkProject: string;
+  categories: string[];
+};
+
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  title,
+  description,
+  imageUrl,
+  linkProject,
+  categories = [],
+}) => {
   return (
     <div className="relative h-[385px] border-[1px] group border-[#e5e5e5] rounded-xl overflow-hidden cursor-pointer transition-all duration-[.5s] hover:scale-[1.03] dark:border-[#404040]">
-      <a aria-label="linkproject" href="#">
+      <a aria-label="linkproject" href={linkProject}>
         <div className="BackgroundHoverT absolute z-[4] h-full w-full bg-black bg-opacity-[0.7] opacity-0 transition-opacity duration-[.5s] group-hover:opacity-80" />
         <div className="flex flex-col items-center">
           <div className="absolute z-0 h-full w-full" />
           <Image
             className="absolute z-[1] transition-all rounded-xl group-hover:blur-[5px] duration-700 ease-in-out scale-100 blur-0 grayscale-0"
-            src="project/bemo.png"
+            src={imageUrl}
             alt="imgproject"
             width={385}
             height={385}
@@ -28,23 +42,21 @@ const ProjectCard = () => {
         </div>
         <div className="font-sora bg-[#fafafa] h-full w-full absolute z-[6] mt-44 px-4 pr-1">
           <div className="flex gap-2 text-[11px] text-white pt-4">
-            <h1 className="flex justify-center bg-[#525252] py-[2px] px-[10px] rounded-xl  border ">
-              #webapps
-            </h1>
-            <h1 className="flex justify-center bg-[#525252] py-[2px] px-[10px] rounded-xl  border ">
-              #ecommerce
-            </h1>
-            <h1 className="flex justify-center bg-[#525252] py-[2px] px-[10px] rounded-xl  border ">
-              #car
-            </h1>
+            {categories.map((category, index) => (
+              <h1
+                key={index}
+                className="flex justify-center bg-[#525252] py-[2px] px-[10px] rounded-xl  border "
+              >
+                #{category}
+              </h1>
+            ))}
           </div>
           <div className="font-sora mt-2">
             <div className="text-[#121212] leading-10 text-lg font-medium transition-all duration-[.5s] group-hover:underline">
-              <h1>BeMO ( Beli Mobil Online)</h1>
+              <h1>{title}</h1>
             </div>
             <p className="text-sm text-[#404040] mt-1 dark:text-[#a3a3a3]">
-              BeMO is an e-commerce platform for buying and selling with a
-              variety of new and used cars.
+              {description}
             </p>
           </div>
         </div>
