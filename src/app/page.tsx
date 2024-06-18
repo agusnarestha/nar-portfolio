1ad7ef669614d15858c1b8ce159634386480db80
Agus Narestha <agusnaresthaa@gmail.com>
2024-06-18 13:34:55 +0800

    fix: missing import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import HomeSection from "@/components/sections/HomeSection";
 import RecentProjectSection from "@/components/sections/RecentProjectSection";
-import AboutSection from "@/components/sections/AboutSection";
 
 export default function Home() {
   return (
