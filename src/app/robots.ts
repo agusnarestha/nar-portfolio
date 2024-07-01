72ca64fe063347b341267193b93bdf1e02c9695f
Agus Narestha <agusnaresthaa@gmail.com>
2024-07-01 11:35:46 +0800

    add: robots.txt

---
+++ src/app/robots.ts
@@ -0,0 +1,12 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
+    sitemap: "https://agusnarestha.github.io/nar-portfolio/sitemap.xml",
+  };
+}
